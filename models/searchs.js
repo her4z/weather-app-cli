b244d042c50ce39f92c8ee08473531fa606225f3
Sebastian Heras <her4z@pop-os.localdomain>
2023-03-20 02:33:40 -0300

    node-fetch test

--- models/searchs.js
+++ models/searchs.js
@@ -1,10 +1,18 @@
+import fetch from "node-fetch";
 export default class Searchs {
   history = ["Madrid", "Buenos Aires", "Malaga"];
   constructor() {}
 
   async search(input = "") {
     console.log(input);
-
-    return [];
+    try {
+      fetch("https://reqres.in/api/users?page=2").then((res) => {
+        res.json().then((data) => {
+          console.log(data);
+        });
+      });
+    } catch (err) {
+      return [];
+    }
   }
 }
